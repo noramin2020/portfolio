12104cd2b20e4e1e80e55921259ca1697ba344c5
Nor-Amin <hadjiali.nm58@s.msumain.edu.ph>
2025-05-24 18:09:04 +0800

    Initial commit with portfolio project

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,79 @@
 import './App.css';
 import profileImg from './assets/pp.jpg';
-import Guess from './assets/guesswifi.png';
-import LPP from './assets/lppchat.png';
-import BRGY from './assets/brgysystem.png';
+import Guess from './assets/Wfi.png';
+import LPP from './assets/LPP.png';
+import BRGY from './assets/Brgy.png';
+import lppp from './assets/lppp.png';
+import Gppp from './assets/guess_logo.png';
+import Bppp from './assets/logo.png';
+import Cppp from './assets/Cursor.png';
+import MSU from './assets/MSU.png';
+import Image from './assets/profile.png';
 import { FaLinkedin, FaInstagram, FaGithub, FaFacebook } from 'react-icons/fa';
+import React, { useState } from "react";
 
 function App() {
+
+   const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = '/UpResume.pdf'; // This should match the filename in your public folder
+    link.download = 'NorAmin_CV.pdf'; // Optional: customize download name
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    
+  };
+
+  
+
   return (
     <div className="app">
       <header className="header">
         <div className="navbar">
           <div className="logo">Nor-Amin Portfolio</div>
-          <nav>
+
+          {/* Hamburger icon */}
+          <div className="burger" onClick={toggleMenu}>
+            &#9776;
+          </div>
+
+          <nav className={`nav-menu ${menuOpen ? "open" : ""}`}>
             <ul>
-            <li><a href="#">Home</a></li>
-              <li><a href="#about">About Me</a></li>
-              <li><a href="#portfolio">Porfolio</a></li>
-              <li><a href="#experience">Experience</a></li>
-              <li><a href="#certificates">Certificates</a></li>
-              <li><a href="#contact">Contact Me</a></li>
+              <li><a href="#" onClick={() => setMenuOpen(false)}>Home</a></li>
+              <li><a href="#about" onClick={() => setMenuOpen(false)}>About Me</a></li>
+              <li><a href="#portfolio" onClick={() => setMenuOpen(false)}>Portfolio</a></li>
+              <li><a href="#experience" onClick={() => setMenuOpen(false)}>Experience</a></li>
+              <li><a href="#skill" onClick={() => setMenuOpen(false)}>Skills</a></li>
+              <li><a href="#contact" onClick={() => setMenuOpen(false)}>Contact Me</a></li>
             </ul>
           </nav>
         </div>
+
         <div className="intro-section">
           <h3>Hello & Welcome</h3>
           <h1>I am A Software Developer | UI/UX Designer</h1>
-          <div className="socials">
-            <FaLinkedin />
-            <FaInstagram />
-            <FaGithub />
-            <FaFacebook />
-          </div>
-          <button className="cv-btn">Download CV</button>
-        </div>
-      </header>
+           <div className="socials">
+        <a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://www.instagram.com/jd_ali_29/profilecard/?igsh=MTNjN3c3a3J3bThlaQ==" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+        <a href="https://github.com/noramin2020" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://www.facebook.com/Light002911" target="_blank" rel="noopener noreferrer">
+          <FaFacebook />
+        </a>
+      </div>
+      <button className="cv-btn" onClick={handleDownload}>Download CV</button>
+      </div>
+    </header>
 
       <section className="about" id="about">
         <div className="about-content">
@@ -60,76 +100,159 @@ function App() {
         <h2>Portfolio</h2>
         <div className="project-card">
         <div class="project-text">
-    <h3>GUESS WI-FI</h3>
-    <h4>Developed a web-based system for secured Wi-Fi access in CICS by configuring a MikroTik router and integrating API-based user authentication. Implemented user access control and blacklist management to enhance network security and ensure efficient user management.</h4>
+    <h3>Barangay Management System</h3>
+    <h4>Developed a web-based system for resident profiling, certificate generation, and 
+QR code encryption for document authentication. Enabled A4-size PDF 
+downloads of generated certificates with embedded encrypted QR codes, 
+ensuring secure and efficient management of barangay records.
+</h4>
   </div>
+  <img className="logo" src={Bppp} alt="GUESS WIFI" />
+   <img src={BRGY} alt="BRGY System" />
+          
+        </div>
+
+        <div className="project-card-LPP">
+          <img src={LPP} alt="LPP GC Chat" />
+        <div class="Lroject-text">
+    <h3>Redesign Launchpad Website</h3>
+    <h4>Redesign the UI of the main websites of the Company using Figma, and Canva for Logos.
+      
+</h4>
+<h4>
+https://thelaunchpadteam.com/
+</h4>
+  </div>
+  <a href="https://thelaunchpadteam.com/" target="_blank" rel="noopener noreferrer">
+  <img className="logo" src={lppp} alt="GUESS WIFI" />
+</a>
+
+        </div>
+
+        <div className="project-card">
+        <div class="project-text">
+    <h3>Wi-fi Access Control System 
+for Learnings of Students
+</h3>
+    <h4>Developed a web-based system for secured Wi-Fi access in CICS by configuring 
+a MikroTik router and integrating API-based user authentication. Implemented 
+user access control and blacklist management to enhance network security and 
+ensure efficient user management.</h4>
+  </div>
+         <img className="logo" src={Gppp} alt="GUESS WIFI" />
           <img src={Guess} alt="GUESS WIFI" />
           
         </div>
-
-        <div className="project-card">
-        <div class="project-text">
-    <h3>GUESS WI-FI</h3>
-    <h4>Developed a web-based system for secured Wi-Fi access in CICS by configuring a MikroTik router and integrating API-based user authentication. Implemented user access control and blacklist management to enhance network security and ensure efficient user management.</h4>
-  </div>
-          <img src={LPP} alt="LPP GC Chat" />
-          
-        </div>
-        <div className="project-card">
-        <div class="project-text">
-    <h3>GUESS WI-FI</h3>
-    <h4>Developed a web-based system for secured Wi-Fi access in CICS by configuring a MikroTik router and integrating API-based user authentication. Implemented user access control and blacklist management to enhance network security and ensure efficient user management.</h4>
-  </div>
-          <img src={BRGY} alt="BRGY System" />
-          
-        </div>
       </section>
 
-      <section className="experience" id="experience">
-        <h2>Experience and Education</h2>
-        <div className="exp-cards">
-          <div className="exp-card">
-            <h4>The Launchpadteam Inc.</h4>
-            <p>Roles: IT Intern</p>
-            <p>Software Developer<br />Wordpress Developer<br />UI/UX Developer<br />IT-Support</p>
-          </div>
-          <div className="exp-card">
-            <h4>The Cursor Publication</h4>
-            <p>Roles: Member</p>
-            <p>Former Cartoon Editor<br />Cartoonist</p>
-          </div>
-          <div className="exp-card">
-            <h4>Mindanao State University Main Campus</h4>
-            <p>Bachelor of Science in Information Technology</p>
-          </div>
-        </div>
-      </section>
 
-      <section className="skills" id="certificates">
+
+      
+
+     <section className="experience" id="experience">
+    <h2>Experience and Education</h2>
+    <div className="exp-cards">
+        <div className="exp-card">
+            <img src={lppp} alt="The Launchpadteam Inc." className="exp-image" />
+            <div className="exp-text">
+                <h4>The Launchpadteam Inc.</h4>
+                <p>Roles: IT Intern</p>
+                <p>Software Developer<br />Wordpress Developer<br />UI/UX Developer<br />IT-Support</p>
+            </div>
+        </div>
+        <div className="exp-card">
+            <img src={Cppp} alt="The Cursor Publication" className="exp-image" />
+            <div className="exp-text">
+                <h4>The Cursor Publication</h4>
+                <p>Roles: Member</p>
+                <p>Former Cartoon Editor<br />Cartoonist</p>
+            </div>
+        </div>
+        <div className="exp-card">
+            <img src={MSU} alt="Mindanao State University Main Campus" className="exp-image" />
+            <div className="exp-text">
+                <h4>Mindanao State University Main Campus</h4>
+                <p>Roles: Student</p>
+                <p>Bachelor of Science in Information Technology<br />2020-2025</p>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+
+      <section className="skills" id="skill">
         <h2>Skills</h2>
         <div className="skills-grid">
-          <div className="skill">Vite</div>
-          <div className="skill">React</div>
-          <div className="skill">PHP</div>
-          <div className="skill">MySQL</div>
-          <div className="skill">HTML</div>
-          <div className="skill">CSS</div>
-          <div className="skill">JavaScript</div>
-          <div className="skill">MikroTik</div>
-          <div className="skill">Canva</div>
-        </div>
+  <div className="skill">
+    <img src="/logos/Vitejs-logo.svg.png" alt="Vite" />
+    <span>Vite</span>
+  </div>
+  <div className="skill">
+    <img src="/logos/react.png" alt="React" />
+    <span>React</span>
+  </div>
+  <div className="skill">
+    <img src="/logos/php.png" alt="PHP" />
+    <span>PHP</span>
+  </div>
+  <div className="skill">
+    <img src="/logos/mySql.png" alt="MySQL" />
+    <span>MySQL</span>
+  </div>
+  <div className="skill">
+    <img src="/logos/html.png" alt="HTML" />
+    <span>HTML</span>
+  </div>
+  <div className="skill">
+    <img src="/logos/css.png" alt="CSS" />
+    <span>CSS</span>
+  </div>
+  <div className="skill">
+    <img src="/logos/js.png" alt="JavaScript" />
+    <span>JavaScript</span>
+  </div>
+  <div className="skill">
+    <img src="/logos/mk.png" alt="MikroTik" />
+    <span>MikroTik</span>
+  </div>
+  <div className="skill">
+    <img src="/logos/canva.png" alt="Canva" />
+    <span>Canva</span>
+  </div>
+  <div className="skill">
+    <img src="/logos/figma.png" alt="figma" />
+    <span>Figma</span>
+  </div>
+</div>
+
       </section>
 
       <footer className="contact" id="contact">
-        <p>Email: <a href="mailto:nor.amin.jcb@owasii.com">nor.amin.jcb@owasii.com</a></p>
-        <p>Cagayan de Oro, Misamis Oriental, Philippines</p>
-        <div className="socials">
-          <FaLinkedin />
-          <FaInstagram />
-          <FaGithub />
-          <FaFacebook />
-        </div>
-      </footer>
+
+  <img src={Image} alt="Logo" className="footer-logo" />
+
+  <p>Email: <a href="mailto:hadjiali.nor.amin.job@gmail.com">hadjiali.nor.amin.job@gmail.com</a></p>
+  <p>Cagayan de Oro, Misamis Oriental, Philippines</p>
+
+  <hr className="footer-divider" />
+
+  <div className="footerS">
+    <a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile" target="_blank" rel="noopener noreferrer">
+      <FaLinkedin />
+    </a>
+    <a href="https://www.instagram.com/jd_ali_29/profilecard/?igsh=MTNjN3c3a3J3bThlaQ==" target="_blank" rel="noopener noreferrer">
+      <FaInstagram />
+    </a>
+    <a href="https://github.com/noramin2020" target="_blank" rel="noopener noreferrer">
+      <FaGithub />
+    </a>
+    <a href="https://www.facebook.com/Light002911" target="_blank" rel="noopener noreferrer">
+      <FaFacebook />
+    </a>
+  </div>
+</footer>
+
     </div>
   );
 }
